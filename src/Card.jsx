410f5450166ade39eaf6139d1a65be8149d30932
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-23 14:14:40 -0400

    .

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,21 +1,22 @@
 import { Link } from "react-router-dom"
 import { Container } from "react-bootstrap"
+import './App.css'
 
 function Card() {
   return (
     <div className="p-5" style={{fontFamily: "sans-serif", display: "flex", justifyContent: "center", alignItems: "center", backgroundImage: "linear-gradient(to bottom, #a371ff, #4171c9)"}}>
       <Link className="text-light" to='/' style={{position: "absolute", alignSelf: "flex-start", marginTop: "-5px", marginLeft: "-1750px"}}>{'<- Back'}</Link>
-      <div>
-      <Container>
-      <div style={{backgroundImage: "linear-gradient(to bottom right, #ac571f, #b3b02c)"}}>
+      <div style={{height: "calc(100vh - 100px"}}>
+      <Container style={{marginBottom: "6000px" }}>
+      <div style={{backgroundImage: "linear-gradient(to bottom right, #ac571f, #b3b02c)", height: "350px"}}>
         <div className="photo">
-            <img className="rounded-circle" src="./img/blogpic.jpg"/>
+            <img className="rounded-circle" src="./img/blogpic.jpg" />
         </div>
-        <ul>
+        <ul style={{margin: "20px", marginTop: "65px"}}>
             <li><b>Jay Miller</b></li>
             <Link to="https://github.com/Jaymiller2002?tab=repositories"><li>Full-Stack Developer</li>click to see projects</Link>
         </ul>
-        <div className="social-media-banner">
+        <div style={{marginTop: "80px"}}>
             <Link to="https://github.com/" style={{fontSize: "2rem", color: "purple"}}><i className="fa-brands fa-square-github"></i></Link>
             <Link to= "mailto:jay.miller02@icloud.com" style={{fontSize: "2rem", color: "purple"}}><i className="fa-solid fa-square-envelope"></i></Link>
             <Link to="tel:+8594758431" style={{fontSize: "2rem", color: "purple"}}><i className="fa-solid fa-square-phone"></i></Link>
