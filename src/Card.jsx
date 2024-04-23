d684f2685d92200880ea4126acdd470b85762804
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-23 15:16:44 -0400

    .

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,27 +4,51 @@ import './App.css'
 
 function Card() {
   return (
-    <div className="p-5" style={{fontFamily: "sans-serif", display: "flex", justifyContent: "center", alignItems: "center", backgroundImage: "linear-gradient(to bottom, #a371ff, #4171c9)"}}>
+    <div className="p-5" style={{
+        fontFamily: "sans-serif",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundImage: "linear-gradient(to bottom, #a371ff, #4171c9)"
+        }}>
       <Link className="text-light" to='/' style={{position: "absolute", alignSelf: "flex-start", marginTop: "-5px", marginLeft: "-1000px"}}>{'<- Back'}</Link>
       <div style={{height: "calc(100vh - 100px"}}>
       <Container style={{marginBottom: "6000px" }}>
-      <div style={{backgroundImage: "linear-gradient(to bottom right, #ac571f, #b3b02c)", height: "350px"}}>
+      <div style={{
+        backgroundImage: "linear-gradient(to bottom right, #ac571f, #b3b02c)",
+        height: "350px"}}>
         <div className="photo">
-            <img className="rounded-circle" src="./img/blogpic.jpg" />
+            <img className="rounded-circle" src="blogpic.jpg"/>
         </div>
         <ul style={{margin: "20px", marginTop: "65px"}}>
             <li><b>Jay Miller</b></li>
             <Link to="https://github.com/Jaymiller2002?tab=repositories"><li>Full-Stack Developer</li>click to see projects</Link>
         </ul>
         <div style={{marginTop: "80px"}}>
-            <Link to="https://github.com/" style={{fontSize: "2rem", color: "purple"}}><i className="fa-brands fa-square-github"></i></Link>
-            <Link to= "mailto:jay.miller02@icloud.com" style={{fontSize: "2rem", color: "purple"}}><i className="fa-solid fa-square-envelope"></i></Link>
-            <Link to="tel:+8594758431" style={{fontSize: "2rem", color: "purple"}}><i className="fa-solid fa-square-phone"></i></Link>
-            <Link to="https://www.linkedin.com/feed/" style={{fontSize: "2rem", color: "purple"}}><i className="fa-brands fa-linkedin"></i></Link>
-            <Link to="https://www.instagram.com/" style={{fontSize: "2rem", color: "purple"}}><i className="fa-brands fa-square-instagram"></i></Link>
-            <Link to="https://twitter.com/" style={{fontSize: "2rem", color: "purple"}}><i className="fa-brands fa-square-x-twitter"></i></Link>
-            <Link to="https://www.youtube.com/" style={{fontSize: "2rem", color: "purple"}}><i className="fa-brands fa-square-youtube"></i></Link>
-            <Link to="https://www.google.com/" style={{fontSize: "2rem", color: "purple"}}><i className="fa-brands fa-square-google-plus"></i></Link>
+            <Link to="https://github.com/" style={{
+              fontSize: "2rem", 
+              color: "purple"}}><i className="fa-brands fa-square-github"></i></Link>
+            <Link to= "mailto:jay.miller02@icloud.com" style={{
+              fontSize: "2rem", 
+              color: "purple"}}><i className="fa-solid fa-square-envelope"></i></Link>
+            <Link to="tel:+8594758431" style={{
+              fontSize: "2rem", 
+              color: "purple"}}><i className="fa-solid fa-square-phone"></i></Link>
+            <Link to="https://www.linkedin.com/feed/" style={{
+              fontSize: "2rem", 
+              color: "purple"}}><i className="fa-brands fa-linkedin"></i></Link>
+            <Link to="https://www.instagram.com/" style={{
+              fontSize: "2rem", 
+              color: "purple"}}><i className="fa-brands fa-square-instagram"></i></Link>
+            <Link to="https://twitter.com/" style={{
+              fontSize: "2rem", 
+              color: "purple"}}><i className="fa-brands fa-square-x-twitter"></i></Link>
+            <Link to="https://www.youtube.com/" style={{
+              fontSize: "2rem", 
+              color: "purple"}}><i className="fa-brands fa-square-youtube"></i></Link>
+            <Link to="https://www.google.com/" style={{
+              fontSize: "2rem", 
+              color: "purple"}}><i className="fa-brands fa-square-google-plus"></i></Link>
         </div>
         </div>
         </Container>
